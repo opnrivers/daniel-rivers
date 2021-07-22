12e1af18038fd52beb56b7eb46587eda052a49bc
d-rivers <28294197+d-rivers@users.noreply.github.com>
2021-07-22 12:33:13 +0100

    webpack set up for development & production

--- webpack.common.js/index.js
+++ webpack.common.js/index.js
@@ -1,15 +1,11 @@
 const entry = require('./entry');
 const output = require('./output');
-const devtool = require('./devtool');
-const devServer = require('./dev-server');
 const rules = require('./rules');
 const plugins = require('./plugins.js');
 
 module.exports = {
   entry,
   output,
-  devtool,
-  devServer,
   module: {
     rules,
   },
